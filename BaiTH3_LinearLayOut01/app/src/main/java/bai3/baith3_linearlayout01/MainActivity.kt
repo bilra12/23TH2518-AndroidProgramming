a3fab7df60ac63d61debde28c2249c978d5d261c
thanh <k14qtm21@gmail.com>
2026-08-13 16:52:58 +0700

    hoan thanh

---
+++ BaiTH3_LinearLayOut01/app/src/main/java/bai3/baith3_linearlayout01/MainActivity.kt
@@ -0,0 +1,31 @@
+package bai3.baith3_linearlayout01
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtSo1 = findViewById<EditText>(R.id.edtSo1)
+        val edtSo2 = findViewById<EditText>(R.id.edtSo2)
+        val btnTinhTong = findViewById<Button>(R.id.btnTinhTong)
+        val txtKetQua = findViewById<TextView>(R.id.txtKetQua)
+
+        btnTinhTong.setOnClickListener {
+            val so1 = edtSo1.text.toString().toDoubleOrNull()
+            val so2 = edtSo2.text.toString().toDoubleOrNull()
+
+            if (so1 != null && so2 != null) {
+                txtKetQua.text = "Kết quả: ${so1 + so2}"
+            } else {
+                txtKetQua.text = "Vui lòng nhập đủ 2 số!"
+            }
+        }
+    }
+}
