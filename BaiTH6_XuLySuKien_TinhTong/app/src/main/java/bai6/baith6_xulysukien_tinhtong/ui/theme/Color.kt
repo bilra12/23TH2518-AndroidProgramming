74f0e61aee59df4f757d1d9c6e0de8291c86c174
thanh <k14qtm21@gmail.com>
2026-08-14 13:00:11 +0700

    hoan thanh

---
+++ BaiTH6_XuLySuKien_TinhTong/app/src/main/java/bai6/baith6_xulysukien_tinhtong/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package bai6.baith6_xulysukien_tinhtong.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
